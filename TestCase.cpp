5fd14a334e8a52f038354a5822eef146b69c11e3
Moshmel <33791298+Moshmel@users.noreply.github.com>
2018-06-12 17:39:11 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,16 +1,8 @@
-#include "TestCase.hpp"
-#include <ostream>
-using namespace std;
-
-
-
-
-
 
 
 void TestCase::to_os(string text)
 {
-	_os << _str << ":Failure in test #" << _suc + _fail << " : " << text <<"!"<<endl;
+	_os << _str << ": Failure in test #" << _suc + _fail << " : " << text <<"!"<<endl;
 }
 
 void TestCase::print()
