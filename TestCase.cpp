1f815153fd5fd6af041afd28b597f1c93eabe2cc
Moshmel <33791298+Moshmel@users.noreply.github.com>
2018-06-12 17:14:24 +0300

    Add files via upload

---
+++ TestCase.cpp
@@ -0,0 +1,25 @@
+#include "TestCase.hpp"
+#include <ostream>
+using namespace std;
+
+
+
+
+
+
+
+void TestCase::to_os(string text)
+{
+	_os << _str << ":Failure in test #" << _suc + _fail << " : " << text << endl;
+}
+
+void TestCase::print()
+{
+	_os << _str << ": " << std::to_string(_fail) << " failed, ";
+	_os << to_string(_suc) << " passed, ";
+	_os << to_string(_suc+_fail) << " total." << endl << "---" << endl;
+}
+
+TestCase::~TestCase()
+{
+}
---
+++ TestCase.hpp
@@ -0,0 +1,105 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <ostream>
+
+using namespace std;
+class TestCase
+{
+	int _suc;
+	int _fail;
+	string _str;
+	void to_os(string text);
+protected:
+	ostream& _os;
+public:
+
+	TestCase(string str, ostream& os) :_str(str), _os(os) {
+		_os << str << endl;
+		_suc = 0;
+		_fail = 0;
+
+	};
+	template<typename T>
+	TestCase& check_equal(T a, T b);
+
+	template<typename T>
+	TestCase& check_different(T a, T b);
+
+	template<typename T1, typename T2, typename T3>
+	TestCase& check_function(T1 func, T2 x, T3 y);
+
+	template <typename T>
+	TestCase& check_output(T x, string str);
+	
+	void print();
+	~TestCase();
+
+
+};
+
+template<typename T>
+inline TestCase & TestCase::check_equal(T a, T b)
+{
+	{
+		
+		if (a != b)
+		{
+			_fail++;
+			string str = "error the 2 arguments are not equal";
+			to_os(str);
+		}
+		else _suc++;
+		
+		return*this;
+	}
+}
+
+template<typename T>
+inline TestCase & TestCase::check_different(T a, T b)
+{
+	if (a == b) {
+		_fail++;
+		string s = "error the 2 arguments are not the same";
+		to_os(s);
+	}
+	else _suc++;
+
+	return *this;
+}
+
+template<typename T1, typename T2, typename T3>
+inline TestCase & TestCase::check_function(T1 fu, T2 x, T3 y)
+{
+	T2 temp = fu(x);
+	if (temp == y)  _suc++;
+	else { _fail++;
+			ostringstream s1, s2;
+			s1 << temp;
+			s2 << y;
+		  string s= "Function should return  " +s2.str() +"instead of  " +s1.str()+"!";
+		  to_os(s);
+		 }
+
+	return *this;
+}
+
+template<typename T>
+inline TestCase & TestCase::check_output(T x, string str)
+{
+	ostringstream out;
+	out << x;
+	if (out.str() == str)
+	{
+		_suc++;
+	}
+	else {
+		_fail++;
+		string s = "string value should be " + str + " but is " + out.str();
+		to_os(s);
+
+	}
+
+	return (*this);
+}
+
